1d7e09f703ac4b7b6413aace42a929fbd4e3b2ef
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2022-12-24 20:57:57 +0530

    Firebase Authenticated

--- login/lib/main.dart
+++ login/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:login/screens/signin_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -24,7 +28,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SignInScreen(),
     );
   }
 }
