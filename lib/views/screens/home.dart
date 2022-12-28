fccb02d9e34c7783f5acff02206bd291519210b4
vsshah2003 <vsshah2003@gmail.com>
2022-12-28 23:29:37 +0530

    Integrated Drawer, menu card widget in home screen

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:smokey_dough/data/pizza_dummy_data.dart';
 import 'package:smokey_dough/views/widgets/home_widget.dart';
+import 'package:smokey_dough/views/widgets/menu_card.dart';
+import 'package:smokey_dough/views/widgets/navbar.dart';
 import 'package:smokey_dough/views/widgets/pizza_card.dart';
 import '../../constants/colors.dart';
 import '../../constants/strings.dart';
@@ -28,140 +30,155 @@ class _HomeScreenViewState extends State<HomeScreenView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        drawer: SafeArea(child: Navbar()),
         body: SafeArea(
-      child: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  HomeWidget(
-                    child: IconButton(
-                      icon: Icon(Icons.menu),
-                      onPressed: () {},
-                    ),
-                  ),
-                  HomeWidget(
-                    child: IconButton(
-                      icon: Icon(Icons.notifications),
-                      onPressed: () {},
-                    ),
-                  )
-                ],
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 10),
-                width: double.infinity,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    RichText(
-                      textAlign: TextAlign.center,
-                      text: TextSpan(
-                          style: GoogleFonts.notoSerif(
-                              color: black,
-                              wordSpacing: 0.1,
-                              letterSpacing: 0.1,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w700),
-                          children: [
-                            TextSpan(text: "What do you want \n"),
-                            TextSpan(text: "for "),
-                            TextSpan(
-                                text: "Dinner",
-                                style: TextStyle(color: primaryColor))
-                          ]),
-                    ),
-                  ],
-                ),
-              ),
-              HomeWidget(
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      prefixIcon: IconButton(
-                        color: notBlack,
-                        icon: Icon(Icons.search),
-                        onPressed: () {},
-                      ),
-                      hintText: "Search your Food",
-                      hintStyle: GoogleFonts.poppins(),
-                      suffixIcon: IconButton(
-                        color: primaryColor,
-                        icon: Icon(Icons.filter_list_alt),
-                        onPressed: () {},
-                      )),
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text("Categories",
-                  style: GoogleFonts.notoSerif(
-                      color: black,
-                      wordSpacing: 0.1,
-                      letterSpacing: 0.1,
-                      fontSize: 22,
-                      fontWeight: FontWeight.w700)),
-              SingleChildScrollView(
-                  child: Column(
-                children: [
-                  SizedBox(
-                    height: 100,
-                    child: ListView.builder(
-                        physics: BouncingScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: CategoryList.length,
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  current = index;
-                                });
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Builder(
+                          builder: (context) => HomeWidget(
+                            child: IconButton(
+                              icon: Icon(Icons.menu),
+                              onPressed: () {
+                                Scaffold.of(context).openDrawer();
                               },
-                              child: HomeWidget(
-                                  height: 80,
-                                  width: 130,
-                                  isSelected: current == index ? true : false,
-                                  padding: EdgeInsets.symmetric(horizontal: 10),
-                                  child: Text(CategoryList[index],
-                                      style: GoogleFonts.notoSerif(
-                                          color: current == index ? white : black,
-                                          wordSpacing: 0.1,
-                                          letterSpacing: 0.1,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w700))),
                             ),
-                          );
-                        }),
+                          )),
+                      HomeWidget(
+                        child: IconButton(
+                          icon: Icon(Icons.notifications),
+                          onPressed: () {},
+                        ),
+                      )
+                    ],
                   ),
+                  Container(
+                    padding: EdgeInsets.symmetric(vertical: 10),
+                    width: double.infinity,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        RichText(
+                          textAlign: TextAlign.center,
+                          text: TextSpan(
+                              style: GoogleFonts.notoSerif(
+                                  color: black,
+                                  wordSpacing: 0.1,
+                                  letterSpacing: 0.1,
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.w700),
+                              children: [
+                                TextSpan(text: "What do you want \n"),
+                                TextSpan(text: "for "),
+                                TextSpan(
+                                    text: "Dinner",
+                                    style: TextStyle(color: primaryColor))
+                              ]),
+                        ),
+                      ],
+                    ),
+                  ),
+                  HomeWidget(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          prefixIcon: IconButton(
+                            color: notBlack,
+                            icon: Icon(Icons.search),
+                            onPressed: () {},
+                          ),
+                          hintText: "Search your Food",
+                          hintStyle: GoogleFonts.poppins(),
+                          suffixIcon: IconButton(
+                            color: primaryColor,
+                            icon: Icon(Icons.filter_list_alt),
+                            onPressed: () {},
+                          )),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text("Categories",
+                      style: GoogleFonts.notoSerif(
+                          color: black,
+                          wordSpacing: 0.1,
+                          letterSpacing: 0.1,
+                          fontSize: 22,
+                          fontWeight: FontWeight.w700)),
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 100,
+                        child: ListView.builder(
+                            physics: BouncingScrollPhysics(),
+                            shrinkWrap: true,
+                            itemCount: CategoryList.length,
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(20.0),
+                                child: GestureDetector(
+                                  onTap: () {
+                                    setState(() {
+                                      current = index;
+                                    });
+                                  },
+                                  child: HomeWidget(
+                                      height: 80,
+                                      width: 130,
+                                      isSelected:
+                                          current == index ? true : false,
+                                      padding:
+                                          EdgeInsets.symmetric(horizontal: 10),
+                                      child: Text(CategoryList[index],
+                                          style: GoogleFonts.notoSerif(
+                                              color: current == index
+                                                  ? white
+                                                  : black,
+                                              wordSpacing: 0.1,
+                                              letterSpacing: 0.1,
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w700))),
+                                ),
+                              );
+                            }),
+                      ),
 
-                  //Start Menu Items from Here
+                      //Start Menu Items from Here
 
-                  ListView.builder(
-                    shrinkWrap: true,
-                    itemBuilder: ((context, index) {
-                      return PizzaCardWidget(
-                        name: pizzaData[index].name,
-                        description: pizzaData[index].description,
-                        price: pizzaData[index].price,
-                      );
-                    }),
-                    itemCount: pizzaData.length,
+                      GridView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemBuilder: ((context, index) {
+                          return MenuCard(
+                              name: pizzaData[index].name,
+                              description: pizzaData[index].description,
+                              price: pizzaData[index].price,
+                              img_path: "assets/images/pizza2.jpg");
+                          // return PizzaCardWidget(
+                          //   name: pizzaData[index].name,
+                          //   description: pizzaData[index].description,
+                          //   price: pizzaData[index].price,
+                          // );
+                        }),
+                        itemCount: pizzaData.length,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            childAspectRatio: 0.5, crossAxisCount: 2),
+                      ),
+                    ],
                   ),
                 ],
-              )),
-            ],
+              ),
+            ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
--- lib/views/widgets/menu_card.dart
+++ lib/views/widgets/menu_card.dart
@@ -19,68 +19,76 @@ class MenuCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Container(
-          width: 200,
-          // height: 100,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              //TODO: correct this
-              color: Colors.black),
-          child: Column(
-            children: [
-              SizedBox(height: 30.0),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Image.asset(img_path),
+    return GestureDetector(
+      onTap: (){
+        Navigator.pushNamed(
+            context,
+            '/menu_item',
+            arguments: {
+              'name': name,
+              'description': description,
+              'price': price}  );
+      },
+      child: Container(
+        padding: EdgeInsets.all(8),
+        margin: EdgeInsets.all(8),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(12),
+            //TODO: correct this
+            color: Colors.black),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              img_path,
+              height: 150,
+              width: 150,
+            ),
+            Text(name,
+                style: GoogleFonts.poppins(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                )),
+            SizedBox(height: 10.0),
+            Text(
+              description,
+              style: GoogleFonts.poppins(
+                fontSize: 12,
+                color: Colors.white,
               ),
-              Padding(
-                padding: EdgeInsets.all(8.0),
-                child: Column(
-                  // crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(name,
-                        style: GoogleFonts.poppins(
-                          fontSize: 30,
-                          color: Colors.white,
-                          fontWeight: FontWeight.w600,
-                        )),
-                    SizedBox(height: 10.0),
-                    Text(
-                      description,
-                      style: GoogleFonts.poppins(
-                        fontSize: 8,
-                        color: Colors.white,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              //  PRICE TAH
+            ),
 
-              SizedBox(height: 30,),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30),
-                child: Container(
-                  decoration: BoxDecoration(color: Colors.white,
-                  borderRadius: BorderRadius.circular(30)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Padding(padding: EdgeInsets.only(left: 25),
-                          child: Text('₹ '+price.toString(),
-                          style: GoogleFonts.poppins(fontSize: 39,
-                          fontWeight: FontWeight.w600),),
-                      ),
-                      SizedBox(height: 35,),
-                      Icon(Icons.add,),
-                    ],
+            //  PRICE TAG
+
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 15),
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(30)),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(left: 25),
+                    child: Text(
+                      '₹ ' + price.toString(),
+                      style: GoogleFonts.poppins(
+                          fontSize: 20, fontWeight: FontWeight.w600),
+                    ),
                   ),
-                ),
-              )
-            ],
-          ),
-        ));
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Icon(
+                    Icons.add,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
